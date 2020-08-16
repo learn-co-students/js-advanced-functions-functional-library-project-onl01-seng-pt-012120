7e52b20431e8e1c97a6e6deb051529a545a08d00
LeonorPDX <leonoracolbert@gmail.com>
2020-08-16 12:24:17 -0700

    Done.

--- index.js
+++ index.js
@@ -88,29 +88,57 @@ const fi = (function() {
       return truthyArr
     },
 
-    sortBy: function(arr, fn) {
-      let sorted = {}; // Set empty object to hold sorts
-      
-      for (let i=0; i < arr.length; i++) { // Run each element of array through function, save to sorts object with index and value
-        sorted[i] = fn(arr[i])
-      };
-      let sortValues = Object.values(sorted); // Make an array of the values after running through function
-      sortValues.sort(); // Sort the VALUES of sorts obj
-      
-      let results = []; // Make empty arr for results
-      
-      for (let i=0; i < sortValues.length; i++) { // For each SORT value
-        let x = Object.keys(sorted).find(k => sorted[k] === sortValues[i]) // find the original index 'x' from the key in sorted object
-        results.push(arr[x]) // push 'x' index from original array into the results arr
-      };
+    sortBy: function(collection, callback) {
+      const newArr = [...collection] // Spread the collection into an array, so our modifications do not affect the original array
+      return newArr.sort(function(a, b) { // Sort with compare function
+        return callback(a) - callback(b) // Sort based on callback function supplied in argument
+      })
     },
 
-    flatten: function() {
-
+    flatten: function(collection, shallow, newArr = []) { // Need newArr in the arguments because we call this function INSIDE this function and need to be able to pass the newArr argument into it
+      if (!Array.isArray(collection)) return newArr.push(collection) // If the collection is not an array, push it into an array and return it
+      if (shallow) { // if shallow is true then flatten only one level
+        for (let val of collection) // Uses for...of to repeat for-loop over iterable elements of collection
+          Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val) // If the element is an array, unpack it. Else push it into the new array
+      } else {
+        for (let val of collection) {
+          this.flatten(val, false, newArr) // For a deep flatten, call THIS FLATTEN FUNCTION on each element
+        }
+      }
+      return newArr // Return the newArr
     },
 
-    uniq: function() {
+    unpack: function(receiver, arr) { // Send the unpack function the newArr and the arr to unpack
+      for (let val of arr) // for each element of the arr
+        receiver.push(val) // Push it into newArr
+    },
 
+    uniqSorted: function(collection) {
+      const sorted = [collection[0]] // Set a variable to an array hold only the first element in the collection
+      for (let i = 1; i < collection.length; i++) { // For loop with i-counter set to 1 (because element at index 0 is in sorted variable)
+        if (sorted[i-1] !== collection[i])  // If the element in the sorted array 1 index behind the collection index is NOT the same,
+          sorted.push(collection[i]) // Push the collection element into the sorted arr
+      }
+      return sorted // returns a sorted array that has only had the unique elements pushed into it
+    },
+
+    uniq: function(collection, sorted=false, iteratee=false) {
+      if (sorted) { // If the collection is already sorted, call the unique sorted (fi.uniqSorted) method and pass it the collection
+        return fi.uniqSorted(collection)
+      } else if (!iteratee) { // Else if there's no callback function, return Set from...
+        return Array.from(new Set(collection)) // Set makes a UNIQUE collection, each value only occurs once. Makes a new array from the Set of the collection
+      } else { // Else if the collection is NOT already sorted, and there IS a callback function...
+        const modifiedVals = new Set() //Create 2 new sets to hold the unique values (unmodified from the original array) and modified values
+        const uniqVals = new Set()
+        for (let val of collection) { // For each iterable in the collection
+          const moddedVal = iteratee(val) // Invoke the callback function on the value and save it to the moddedVal variable
+          if (!modifiedVals.has(moddedVal)) { // If the modifiedVals set does NOT have the moddedVal
+            modifiedVals.add(moddedVal) // Add the new moddedVal to the set
+            uniqVals.add(val) // Add the original, unmodified val to the uniqVals set
+          }
+        }
+        return Array.from(uniqVals) // Return the uniqVals as an array, unmodified values, but filtered by whether their RESULTS through the callback were unique
+      }
     },
 
     keys: function(obj) {
